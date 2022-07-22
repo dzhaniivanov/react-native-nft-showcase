bebc2f26a26ac32862b7eadf410c40ba09716b5b
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-07-22 17:56:50 +0300

    work on component structure

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { Download, SectionWrapper, Features } from "./components";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <SectionWrapper
+        title="Your own store of Nifty NTFs.Start Selling & Growing"
+        description="Buy,store,collect NFTs,exchange & earn crypto.Join 25+ million people using ProNef Marketplace"
+      />
+    </>
   );
-}
+};
 
 export default App;
